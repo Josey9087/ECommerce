1e17307093ab9abfaff0f765ae9cc57197342917
Josey9087 <88466226+Josey9087@users.noreply.github.com>
2022-01-24 14:50:16 -0700

    adding get routes to product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,25 +4,45 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
+  try {
+    const products = await Product.findAll({include: [{ model: Category }, { model: Tag }]});
+    res.json(products);
+  } catch(err) {
+    console.log("Wrong");
   // be sure to include its associated Category and Tag data
-});
+}});
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
+  try {
+    const product = await Product.findByPk(req.params.id, { include: [{ model: Tag }, { model: Category}] });
+
+
+    if (!product) {
+      res.status(404).json({ message: 'Product not found' });
+      return;
+    }
+
+    res.json(product);
+
+  } catch (err) {
+    console.log("Wrong");
+    // be sure to include its associated Products
+  }
   // be sure to include its associated Category and Tag data
 });
 
 // create new product
 router.post('/', (req, res) => {
   /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
+ {
+      "product_name": "Basketball",
+      "price": 200.00,
+      "stock": 3,
+      "tagIds": [1, 2, 3, 4]
     }
   */
   Product.create(req.body)
