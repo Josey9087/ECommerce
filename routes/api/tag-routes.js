a209fab553e96442a0fde9dd003de7645a977973
Josey9087 <88466226+Josey9087@users.noreply.github.com>
2022-01-28 23:40:25 -0700

    adding get eoute for id and all tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,11 +5,32 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', async (req, res) => {
   // find all tags
+  try {
+    const tag = await Tag.findAll({ include: [{ model: Product }] });
+    res.json(tag);
+  } catch (err) {
+    console.log("Wrong");
+    // be sure to include its associated Products
+  }
   // be sure to include its associated Product data
 });
 
 router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
+  try {
+    const tag = await Tag.findByPk(req.params.id, { include: [{ model: Product }] });
+
+
+    if (!tag) {
+      res.status(404).json({ message: 'Tag not found' });
+      return;
+    }
+
+    res.json(category);
+
+  } catch (err) {
+    console.log("Wrong");
+  }
   // be sure to include its associated Product data
 });
 
