00f2f9bff9485c412b691a9b44e1f203cd7bc456
Josey9087 <88466226+Josey9087@users.noreply.github.com>
2022-01-28 23:55:46 -0700

    adding put route for id in tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -36,6 +36,13 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const newtag = await Tag.create({tag_name:req.body});
+    res.status(200).json(newtag);
+
+} catch (err) {
+    res.status(500).json(err);
+}
 });
 
 router.put('/:id', async (req, res) => {
